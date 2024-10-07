b50f24698a77eaf0659f9b9babaf6cb9123f045f
Howl <24992550+howlagon@users.noreply.github.com>
2024-10-06 23:03:51 -0400

    a

--- main.py
+++ main.py
@@ -8,27 +8,56 @@
 # ///
 
 import pygame
+import pygame_gui
 import asyncio
-from pygame_gui.core.utility import get_default_manager
-
-from debug_menu import debugConsole
-from screen import load_manager
-from manager import MANAGER
 
 pygame.init()
 
 window = pygame.display.set_mode((800, 700))
 clock = pygame.time.Clock()
-if MANAGER is None:
-    MANAGER = load_manager(res=(800, 700), screen_offset=(0, 0), scale=1)
 
-debug_console = debugConsole(pygame.rect.Rect(0, 0, 800, 700), MANAGER)
+manager = pygame_gui.UIManager((800, 700))
+
+pause_button = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect((350, 275), (100, 50)),
+    text='Pause',
+    manager=manager
+)
+
+paused = False
+
+status = pygame_gui.elements.UILabel(
+    relative_rect=pygame.Rect((350, 325), (100, 50)),
+    text='Status: ?',
+    manager=manager
+)
 
 async def main():
     while True:
+        delta = clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == pause_button:
+                        global paused
+                        paused = not paused
+                        pause_button.set_text('Resume' if paused else 'Pause')
+                        if paused:
+                            pygame.mixer.music.pause()
+                        else:
+                            pygame.mixer.music.unpause()
+            manager.process_events(event)
+        manager.update(delta)
         window.fill((255, 255, 255))
+        manager.draw_ui(window)
+        status.set_text(f'Busy: {pygame.mixer.music.get_busy()}')
+        if not pygame.mixer.music.get_busy() and not paused:
+            pygame.mixer.music.load('Menu_Ambiance.ogg')
+            pygame.mixer.music.play()
         pygame.display.update()
-        clock.tick(60)
         await asyncio.sleep(0)
 
 if __name__ == "__main__":
